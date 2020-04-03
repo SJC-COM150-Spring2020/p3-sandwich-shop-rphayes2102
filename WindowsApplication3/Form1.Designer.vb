2da6150c424f954ff8c5f60e2e0361b88fa01920
rphayes2102 <60491812+rphayes2102@users.noreply.github.com>
2020-04-02 21:11:00 -0400

    Egg Sandwich Shop

---
+++ WindowsApplication3/Form1.Designer.vb
@@ -0,0 +1,368 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.GrpBoxDrink = New System.Windows.Forms.GroupBox()
+        Me.GrpBoxExtra = New System.Windows.Forms.GroupBox()
+        Me.GrpBoxEggs = New System.Windows.Forms.GroupBox()
+        Me.LblDrink = New System.Windows.Forms.Label()
+        Me.ChkBoxMilk = New System.Windows.Forms.CheckBox()
+        Me.ChkBoxCoffee = New System.Windows.Forms.CheckBox()
+        Me.ChkBoxTea = New System.Windows.Forms.CheckBox()
+        Me.ChkBoxHot = New System.Windows.Forms.CheckBox()
+        Me.ChkBoxJuice = New System.Windows.Forms.CheckBox()
+        Me.ChkBoxBacon = New System.Windows.Forms.CheckBox()
+        Me.Lblextra = New System.Windows.Forms.Label()
+        Me.ChkBoxSausage = New System.Windows.Forms.CheckBox()
+        Me.ChkBoxHam = New System.Windows.Forms.CheckBox()
+        Me.ChkBoxCheese = New System.Windows.Forms.CheckBox()
+        Me.ChkBoxVeggie = New System.Windows.Forms.CheckBox()
+        Me.radBtnOver = New System.Windows.Forms.RadioButton()
+        Me.radBtnScram = New System.Windows.Forms.RadioButton()
+        Me.radBtnSunny = New System.Windows.Forms.RadioButton()
+        Me.radBtnPoached = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lstBoxOrder = New System.Windows.Forms.ListBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnOrder = New System.Windows.Forms.Button()
+        Me.GrpBoxDrink.SuspendLayout()
+        Me.GrpBoxExtra.SuspendLayout()
+        Me.GrpBoxEggs.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GrpBoxDrink
+        '
+        Me.GrpBoxDrink.Controls.Add(Me.ChkBoxJuice)
+        Me.GrpBoxDrink.Controls.Add(Me.ChkBoxHot)
+        Me.GrpBoxDrink.Controls.Add(Me.ChkBoxTea)
+        Me.GrpBoxDrink.Controls.Add(Me.ChkBoxCoffee)
+        Me.GrpBoxDrink.Controls.Add(Me.ChkBoxMilk)
+        Me.GrpBoxDrink.Controls.Add(Me.LblDrink)
+        Me.GrpBoxDrink.Font = New System.Drawing.Font("MV Boli", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GrpBoxDrink.Location = New System.Drawing.Point(43, 66)
+        Me.GrpBoxDrink.Name = "GrpBoxDrink"
+        Me.GrpBoxDrink.Size = New System.Drawing.Size(213, 185)
+        Me.GrpBoxDrink.TabIndex = 0
+        Me.GrpBoxDrink.TabStop = False
+        Me.GrpBoxDrink.Text = "Drinks"
+        '
+        'GrpBoxExtra
+        '
+        Me.GrpBoxExtra.Controls.Add(Me.ChkBoxVeggie)
+        Me.GrpBoxExtra.Controls.Add(Me.ChkBoxCheese)
+        Me.GrpBoxExtra.Controls.Add(Me.ChkBoxHam)
+        Me.GrpBoxExtra.Controls.Add(Me.ChkBoxSausage)
+        Me.GrpBoxExtra.Controls.Add(Me.Lblextra)
+        Me.GrpBoxExtra.Controls.Add(Me.ChkBoxBacon)
+        Me.GrpBoxExtra.Font = New System.Drawing.Font("MV Boli", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GrpBoxExtra.Location = New System.Drawing.Point(302, 66)
+        Me.GrpBoxExtra.Name = "GrpBoxExtra"
+        Me.GrpBoxExtra.Size = New System.Drawing.Size(228, 185)
+        Me.GrpBoxExtra.TabIndex = 1
+        Me.GrpBoxExtra.TabStop = False
+        Me.GrpBoxExtra.Text = "Extras"
+        '
+        'GrpBoxEggs
+        '
+        Me.GrpBoxEggs.Controls.Add(Me.Label1)
+        Me.GrpBoxEggs.Controls.Add(Me.radBtnPoached)
+        Me.GrpBoxEggs.Controls.Add(Me.radBtnSunny)
+        Me.GrpBoxEggs.Controls.Add(Me.radBtnScram)
+        Me.GrpBoxEggs.Controls.Add(Me.radBtnOver)
+        Me.GrpBoxEggs.Font = New System.Drawing.Font("MV Boli", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GrpBoxEggs.Location = New System.Drawing.Point(43, 293)
+        Me.GrpBoxEggs.Name = "GrpBoxEggs"
+        Me.GrpBoxEggs.Size = New System.Drawing.Size(219, 186)
+        Me.GrpBoxEggs.TabIndex = 2
+        Me.GrpBoxEggs.TabStop = False
+        Me.GrpBoxEggs.Text = "Eggs"
+        '
+        'LblDrink
+        '
+        Me.LblDrink.AutoSize = True
+        Me.LblDrink.Font = New System.Drawing.Font("MV Boli", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblDrink.Location = New System.Drawing.Point(74, 16)
+        Me.LblDrink.Name = "LblDrink"
+        Me.LblDrink.Size = New System.Drawing.Size(75, 25)
+        Me.LblDrink.TabIndex = 0
+        Me.LblDrink.Text = "Drinks "
+        '
+        'ChkBoxMilk
+        '
+        Me.ChkBoxMilk.AutoSize = True
+        Me.ChkBoxMilk.Font = New System.Drawing.Font("MV Boli", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ChkBoxMilk.Location = New System.Drawing.Point(26, 47)
+        Me.ChkBoxMilk.Name = "ChkBoxMilk"
+        Me.ChkBoxMilk.Size = New System.Drawing.Size(48, 20)
+        Me.ChkBoxMilk.TabIndex = 1
+        Me.ChkBoxMilk.Text = "Milk"
+        Me.ChkBoxMilk.UseVisualStyleBackColor = True
+        '
+        'ChkBoxCoffee
+        '
+        Me.ChkBoxCoffee.AutoSize = True
+        Me.ChkBoxCoffee.Font = New System.Drawing.Font("MV Boli", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ChkBoxCoffee.Location = New System.Drawing.Point(26, 71)
+        Me.ChkBoxCoffee.Name = "ChkBoxCoffee"
+        Me.ChkBoxCoffee.Size = New System.Drawing.Size(58, 20)
+        Me.ChkBoxCoffee.TabIndex = 2
+        Me.ChkBoxCoffee.Text = "Coffee"
+        Me.ChkBoxCoffee.UseVisualStyleBackColor = True
+        '
+        'ChkBoxTea
+        '
+        Me.ChkBoxTea.AutoSize = True
+        Me.ChkBoxTea.Font = New System.Drawing.Font("MV Boli", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ChkBoxTea.Location = New System.Drawing.Point(26, 95)
+        Me.ChkBoxTea.Name = "ChkBoxTea"
+        Me.ChkBoxTea.Size = New System.Drawing.Size(44, 20)
+        Me.ChkBoxTea.TabIndex = 3
+        Me.ChkBoxTea.Text = "Tea"
+        Me.ChkBoxTea.UseVisualStyleBackColor = True
+        '
+        'ChkBoxHot
+        '
+        Me.ChkBoxHot.AutoSize = True
+        Me.ChkBoxHot.Font = New System.Drawing.Font("MV Boli", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ChkBoxHot.Location = New System.Drawing.Point(26, 119)
+        Me.ChkBoxHot.Name = "ChkBoxHot"
+        Me.ChkBoxHot.Size = New System.Drawing.Size(101, 20)
+        Me.ChkBoxHot.TabIndex = 4
+        Me.ChkBoxHot.Text = "Hot Chocolate"
+        Me.ChkBoxHot.UseVisualStyleBackColor = True
+        '
+        'ChkBoxJuice
+        '
+        Me.ChkBoxJuice.AutoSize = True
+        Me.ChkBoxJuice.Font = New System.Drawing.Font("MV Boli", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ChkBoxJuice.Location = New System.Drawing.Point(26, 143)
+        Me.ChkBoxJuice.Name = "ChkBoxJuice"
+        Me.ChkBoxJuice.Size = New System.Drawing.Size(54, 20)
+        Me.ChkBoxJuice.TabIndex = 5
+        Me.ChkBoxJuice.Text = "Juice"
+        Me.ChkBoxJuice.UseVisualStyleBackColor = True
+        '
+        'ChkBoxBacon
+        '
+        Me.ChkBoxBacon.AutoSize = True
+        Me.ChkBoxBacon.Location = New System.Drawing.Point(27, 47)
+        Me.ChkBoxBacon.Name = "ChkBoxBacon"
+        Me.ChkBoxBacon.Size = New System.Drawing.Size(56, 20)
+        Me.ChkBoxBacon.TabIndex = 0
+        Me.ChkBoxBacon.Text = "Bacon"
+        Me.ChkBoxBacon.UseVisualStyleBackColor = True
+        '
+        'Lblextra
+        '
+        Me.Lblextra.AutoSize = True
+        Me.Lblextra.Font = New System.Drawing.Font("MV Boli", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lblextra.Location = New System.Drawing.Point(86, 16)
+        Me.Lblextra.Name = "Lblextra"
+        Me.Lblextra.Size = New System.Drawing.Size(67, 25)
+        Me.Lblextra.TabIndex = 1
+        Me.Lblextra.Text = "Extras"
+        '
+        'ChkBoxSausage
+        '
+        Me.ChkBoxSausage.AutoSize = True
+        Me.ChkBoxSausage.Location = New System.Drawing.Point(27, 73)
+        Me.ChkBoxSausage.Name = "ChkBoxSausage"
+        Me.ChkBoxSausage.Size = New System.Drawing.Size(65, 20)
+        Me.ChkBoxSausage.TabIndex = 2
+        Me.ChkBoxSausage.Text = "Sausage"
+        Me.ChkBoxSausage.UseVisualStyleBackColor = True
+        '
+        'ChkBoxHam
+        '
+        Me.ChkBoxHam.AutoSize = True
+        Me.ChkBoxHam.Location = New System.Drawing.Point(27, 97)
+        Me.ChkBoxHam.Name = "ChkBoxHam"
+        Me.ChkBoxHam.Size = New System.Drawing.Size(49, 20)
+        Me.ChkBoxHam.TabIndex = 3
+        Me.ChkBoxHam.Text = "Ham"
+        Me.ChkBoxHam.UseVisualStyleBackColor = True
+        '
+        'ChkBoxCheese
+        '
+        Me.ChkBoxCheese.AutoSize = True
+        Me.ChkBoxCheese.Location = New System.Drawing.Point(27, 121)
+        Me.ChkBoxCheese.Name = "ChkBoxCheese"
+        Me.ChkBoxCheese.Size = New System.Drawing.Size(60, 20)
+        Me.ChkBoxCheese.TabIndex = 4
+        Me.ChkBoxCheese.Text = "Cheese"
+        Me.ChkBoxCheese.UseVisualStyleBackColor = True
+        '
+        'ChkBoxVeggie
+        '
+        Me.ChkBoxVeggie.AutoSize = True
+        Me.ChkBoxVeggie.Location = New System.Drawing.Point(27, 145)
+        Me.ChkBoxVeggie.Name = "ChkBoxVeggie"
+        Me.ChkBoxVeggie.Size = New System.Drawing.Size(62, 20)
+        Me.ChkBoxVeggie.TabIndex = 5
+        Me.ChkBoxVeggie.Text = "Veggies"
+        Me.ChkBoxVeggie.UseVisualStyleBackColor = True
+        '
+        'radBtnOver
+        '
+        Me.radBtnOver.AutoSize = True
+        Me.radBtnOver.Location = New System.Drawing.Point(35, 48)
+        Me.radBtnOver.Name = "radBtnOver"
+        Me.radBtnOver.Size = New System.Drawing.Size(77, 20)
+        Me.radBtnOver.TabIndex = 0
+        Me.radBtnOver.TabStop = True
+        Me.radBtnOver.Text = "Over Easy"
+        Me.radBtnOver.UseVisualStyleBackColor = True
+        '
+        'radBtnScram
+        '
+        Me.radBtnScram.AutoSize = True
+        Me.radBtnScram.Location = New System.Drawing.Point(35, 74)
+        Me.radBtnScram.Name = "radBtnScram"
+        Me.radBtnScram.Size = New System.Drawing.Size(77, 20)
+        Me.radBtnScram.TabIndex = 1
+        Me.radBtnScram.TabStop = True
+        Me.radBtnScram.Text = "Scrambled"
+        Me.radBtnScram.UseVisualStyleBackColor = True
+        '
+        'radBtnSunny
+        '
+        Me.radBtnSunny.AutoSize = True
+        Me.radBtnSunny.Location = New System.Drawing.Point(35, 100)
+        Me.radBtnSunny.Name = "radBtnSunny"
+        Me.radBtnSunny.Size = New System.Drawing.Size(102, 20)
+        Me.radBtnSunny.TabIndex = 2
+        Me.radBtnSunny.TabStop = True
+        Me.radBtnSunny.Text = "Sunny Side Up"
+        Me.radBtnSunny.UseVisualStyleBackColor = True
+        '
+        'radBtnPoached
+        '
+        Me.radBtnPoached.AutoSize = True
+        Me.radBtnPoached.Location = New System.Drawing.Point(35, 126)
+        Me.radBtnPoached.Name = "radBtnPoached"
+        Me.radBtnPoached.Size = New System.Drawing.Size(66, 20)
+        Me.radBtnPoached.TabIndex = 3
+        Me.radBtnPoached.TabStop = True
+        Me.radBtnPoached.Text = "Poached"
+        Me.radBtnPoached.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MV Boli", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(83, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 25)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Eggs"
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("MV Boli", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(302, 445)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(115, 34)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'lstBoxOrder
+        '
+        Me.lstBoxOrder.Font = New System.Drawing.Font("MV Boli", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstBoxOrder.FormattingEnabled = True
+        Me.lstBoxOrder.ItemHeight = 21
+        Me.lstBoxOrder.Location = New System.Drawing.Point(364, 264)
+        Me.lstBoxOrder.Name = "lstBoxOrder"
+        Me.lstBoxOrder.Size = New System.Drawing.Size(166, 151)
+        Me.lstBoxOrder.TabIndex = 4
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("MV Boli", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(485, 445)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(106, 34)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnOrder
+        '
+        Me.btnOrder.Location = New System.Drawing.Point(166, 264)
+        Me.btnOrder.Name = "btnOrder"
+        Me.btnOrder.Size = New System.Drawing.Size(131, 23)
+        Me.btnOrder.TabIndex = 6
+        Me.btnOrder.Text = "Order"
+        Me.btnOrder.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Yellow
+        Me.ClientSize = New System.Drawing.Size(614, 491)
+        Me.Controls.Add(Me.btnOrder)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lstBoxOrder)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.GrpBoxEggs)
+        Me.Controls.Add(Me.GrpBoxExtra)
+        Me.Controls.Add(Me.GrpBoxDrink)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GrpBoxDrink.ResumeLayout(False)
+        Me.GrpBoxDrink.PerformLayout()
+        Me.GrpBoxExtra.ResumeLayout(False)
+        Me.GrpBoxExtra.PerformLayout()
+        Me.GrpBoxEggs.ResumeLayout(False)
+        Me.GrpBoxEggs.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GrpBoxDrink As GroupBox
+    Friend WithEvents GrpBoxExtra As GroupBox
+    Friend WithEvents GrpBoxEggs As GroupBox
+    Friend WithEvents LblDrink As Label
+    Friend WithEvents ChkBoxJuice As CheckBox
+    Friend WithEvents ChkBoxHot As CheckBox
+    Friend WithEvents ChkBoxTea As CheckBox
+    Friend WithEvents ChkBoxCoffee As CheckBox
+    Friend WithEvents ChkBoxMilk As CheckBox
+    Friend WithEvents Lblextra As Label
+    Friend WithEvents ChkBoxBacon As CheckBox
+    Friend WithEvents ChkBoxVeggie As CheckBox
+    Friend WithEvents ChkBoxCheese As CheckBox
+    Friend WithEvents ChkBoxHam As CheckBox
+    Friend WithEvents ChkBoxSausage As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents radBtnPoached As RadioButton
+    Friend WithEvents radBtnSunny As RadioButton
+    Friend WithEvents radBtnScram As RadioButton
+    Friend WithEvents radBtnOver As RadioButton
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lstBoxOrder As ListBox
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnOrder As Button
+End Class
---
+++ WindowsApplication3/Form1.vb
@@ -0,0 +1,73 @@
+﻿Public Class Form1
+    Private Sub GroupBox3_Enter(sender As Object, e As EventArgs) Handles GrpBoxEggs.Enter
+
+
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles ChkBoxHam.CheckedChanged
+
+    End Sub
+
+    Private Sub GrpBoxDrink_Enter(sender As Object, e As EventArgs) Handles GrpBoxDrink.Enter
+        'declare variables 
+        Dim drink As String
+        Dim Total As Double
+        Total = 0
+
+        drink = "Your beverage is "
+        If (ChkBoxCoffee.Checked = True) Then
+            lstBoxOrder.Items.Add("Coffee - $2.25")
+            Total = Total + 2.25
+        End If
+        If (ChkBoxJuice.Checked = True) Then
+            lstBoxOrder.Items.Add("Juice - $2.25")
+            Total = Total + 2.25
+        End If
+        If (ChkBoxHot.Checked = True) Then
+            lstBoxOrder.Items.Add("Hot Chocolate - $2.25")
+            Total = Total + 2.25
+        End If
+        If (ChkBoxMilk.Checked = True) Then
+            lstBoxOrder.Items.Add("Milk - $2.25")
+            Total = Total + 2.25
+        End If
+        If (ChkBoxTea.Checked = True) Then
+            lstBoxOrder.Items.Add("Tea - $2.25")
+            Total = Total + 2.25
+        End If
+
+    End Sub
+
+    Private Sub Extras_Enter(sender As Object, e As EventArgs) Handles GrpBoxExtra.Enter
+        'declare variables 
+        Dim Extras As String
+        Dim total As Double
+        total = 0
+
+        Extras = "Your extras are "
+        If (ChkBoxHam.Checked = True) Then
+            lstBoxOrder.Items.Add("Ham - $1.50")
+            total = total + 1.5
+        End If
+        If (ChkBoxBacon.Checked = True) Then
+            lstBoxOrder.Items.Add("Bacon - $1.50")
+            total = total + 1.5
+        End If
+        If (ChkBoxSausage.Checked = True) Then
+            lstBoxOrder.Items.Add("Sausage - $1.50")
+            total = total + 1.5
+        End If
+        If (ChkBoxCheese.Checked = True) Then
+            lstBoxOrder.Items.Add("Cheese - $1.50")
+            total = total + 1.5
+        End If
+        If (ChkBoxVeggie.Checked = True) Then
+            lstBoxOrder.Items.Add("Veggies - $1.50")
+            total = total + 1.5
+        End If
+    End Sub
+
+    Private Sub btnOrder_Click(sender As Object, e As EventArgs) Handles btnOrder.Click
+
+    End Sub
+End Class
